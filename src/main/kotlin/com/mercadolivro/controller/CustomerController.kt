b8cddb2ff75a7f310db87376946514269cc929a7
RCristyan <rcristyan9@gmail.com>
2022-10-06 11:57:19 -0300

    Alterado path de CustomerController

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,14 +1,16 @@
 package com.mercadolivro.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/customer")
 class CustomerController {
 
     @GetMapping
     fun helloWorld() : String {
-        return "Hello world"
+        return "Customer 1"
     }
 
 }
