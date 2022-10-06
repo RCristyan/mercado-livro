75a2c31f3e9d256e8a802dbddae24e37045c971a
RCristyan <rcristyan9@gmail.com>
2022-10-06 12:09:45 -0300

    Adicionado CustomerModel

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,16 +1,17 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld() : String {
-        return "Customer 1"
+    fun getCustomer() : CustomerModel {
+        return CustomerModel(id="1", nome="José Augusto", email="joseaug12@email.com")
     }
 
 }
