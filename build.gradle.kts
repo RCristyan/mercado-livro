7e4a84f0d18d9ef63fd894e99db2f4da920a6715
RCristyan <rcristyan9@gmail.com>
2022-10-10 15:38:18 -0300

    Adicionada migration para criar tabela customer

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.flywaydb:flyway-core:9.4.0")
+	implementation("org.flywaydb:flyway-mysql")
+
 	runtimeOnly("mysql:mysql-connector-java")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
