68dbd3d18b1ef11d56001eb8350c4c8733530e03
RCristyan <rcristyan9@gmail.com>
2022-10-13 13:38:49 -0300

    Refatorado CustomerService.delete()

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
 import org.springframework.stereotype.Service
@@ -36,9 +37,10 @@ class CustomerService(
 
     fun delete(id: Int) {
         val customer = findById(id)
-
         bookService.deleteByCustomer(customer)
-        customerRepository.deleteById(id)
+
+        customer.status = CustomerStatus.INATIVO
+        customerRepository.save(customer)
     }
 
 }
