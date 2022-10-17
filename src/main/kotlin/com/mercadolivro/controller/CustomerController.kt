d0e49ee02cf306ebd5e1a5d9db83ae11ef242695
RCristyan <rcristyan9@gmail.com>
2022-10-17 20:28:14 -0300

    Adicionada paginação em customer

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -6,6 +6,9 @@ import com.mercadolivro.controller.response.CustomerResponse
 import com.mercadolivro.extension.toCustomerModel
 import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.CustomerService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -16,8 +19,8 @@ class CustomerController(
 ) {
 
     @GetMapping
-    fun getAll(@RequestParam nome : String?) : List<CustomerResponse> {
-        return customerService.getAll(nome).map { it.toResponse() }
+    fun getAll(@PageableDefault(page=0, size=10) pageable: Pageable, @RequestParam nome : String?) : Page<CustomerResponse> {
+        return customerService.getAll(pageable, nome).map { it.toResponse() }
     }
 
     @GetMapping("/{id}")
