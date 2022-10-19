2bfcc8710c484c88b9728a83137a2481cca64f50
RCristyan <rcristyan9@gmail.com>
2022-10-19 09:43:46 -0300

    Adicionada validação em PostCustomerRequest

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.flywaydb:flyway-core:9.4.0")
 	implementation("org.flywaydb:flyway-mysql")
 
