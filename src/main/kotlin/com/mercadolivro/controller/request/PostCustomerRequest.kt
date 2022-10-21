cfa307bad77c261087e6add58034ac749ae1ed1a
RCristyan <rcristyan9@gmail.com>
2022-10-21 18:07:59 -0300

    Adicionada validação customizada para disponibilidade de email

--- src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controller.request
 
+import com.mercadolivro.validation.EmailAvailable
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotEmpty
 
@@ -9,5 +10,6 @@ data class PostCustomerRequest(
     var nome: String,
 
     @field:Email(message = "Email deve ser válido")
+    @EmailAvailable
     var email: String
 )
