adc342890a0a45d805ef3c41cebfa2306a3f27f6
RCristyan <rcristyan9@gmail.com>
2022-11-07 11:08:07 -0300

    Implementada lógica para atualizar status de livro para vendido

---
+++ src/main/kotlin/com/mercadolivro/events/listener/UpdateSoldBookListener.kt
@@ -0,0 +1,18 @@
+package com.mercadolivro.events.listener
+
+import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.service.BookService
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+
+@Component
+class UpdateSoldBookListener(
+    private val bookService: BookService
+) {
+
+    @EventListener
+    fun listener(purchaseEvent: PurchaseEvent) {
+        bookService.purchase(purchaseEvent.purchaseModel.books)
+    }
+
+}
--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -57,4 +57,10 @@ class BookService(
     fun findAllByIds(bookIds: Set<Int>): List<BookModel> {
         return bookRepository.findAllById(bookIds).toList()
     }
+
+    fun purchase(books: MutableList<BookModel>) {
+        books.map { it.status = BookStatus.VENDIDO }
+        bookRepository.saveAll(books)
+    }
+
 }
