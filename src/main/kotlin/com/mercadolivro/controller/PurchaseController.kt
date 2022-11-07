800a242e6b7d3d2ffd0178bc6c15ba899c5ee865
RCristyan <rcristyan9@gmail.com>
2022-11-07 12:04:40 -0300

    Lógica de retornar livros comprados pelo cliente movida para PurchaseController

--- src/main/kotlin/com/mercadolivro/controller/PurchaseController.kt
+++ src/main/kotlin/com/mercadolivro/controller/PurchaseController.kt
@@ -2,24 +2,28 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.mapper.PurchaseMapper
 import com.mercadolivro.controller.request.PostPurchaseRequest
+import com.mercadolivro.model.BookModel
+import com.mercadolivro.service.CustomerService
 import com.mercadolivro.service.PurchaseService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("purchases")
 class PurchaseController(
     private val purchaseService: PurchaseService,
-    private val purchaseMapper: PurchaseMapper
+    private val purchaseMapper: PurchaseMapper,
+    private val customerService: CustomerService
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun purchase(@RequestBody request: PostPurchaseRequest){
         purchaseService.create(purchaseMapper.toModel(request))
+    }
+
+    @GetMapping("/{id}")
+    fun getPurchasedBooks(@PathVariable id: Int): List<BookModel> {
+        return customerService.getPurchasedBooks(id)
     }
 }
